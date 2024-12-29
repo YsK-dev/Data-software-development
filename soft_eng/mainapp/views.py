ae18b760d093a676330427f29fd21cb3773b0998
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2024-12-30 00:36:44 +0300

    view functions updated

--- soft_eng/mainapp/views.py
+++ soft_eng/mainapp/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Car, FootballGame, FootballPlayer
-
 
 def home(request):
     return render(request, 'home.html')
 
 def car_list(request):
     cars = Car.objects.all()
-    return render(request, 'car_list.html', {'cars': cars})
+    paginator = Paginator(cars, 100)  # Show 100 cars per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'car_list.html', {'cars': page_obj})
 
 def football_game_list(request):
     games = FootballGame.objects.all()
-    return render(request, 'football_game_list.html', {'games': games})
+    paginator = Paginator(games, 100)  # Show 100 games per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'football_game_list.html', {'games': page_obj})
 
 def football_player_list(request):
     players = FootballPlayer.objects.all()
-    return render(request, 'football_player_list.html', {'players': players})
+    paginator = Paginator(players, 100)  # Show 100 players per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'football_player_list.html', {'players': page_obj})
