c27d532a0e82d76d6f5e6c6092c48fff93cdb12a
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2024-12-30 00:03:10 +0300

    admin panel created

--- soft_eng/mainapp/admin.py
+++ soft_eng/mainapp/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin #type: ignore
 
-# Register your models here.
+from .models import FootballPlayer
+
+
+
+@admin.register(FootballPlayer)
+class FootballPlayerAdmin(admin.ModelAdmin):
+    list_display = ('index', 'Player', 'number', 'Nation', 'Pos', 'Age', 'Min', 'Gls', 'Ast', 'PK', 'PKatt', 'Sh', 'SoT', 'CrdY', 'CrdR', 'Touches', 'Tkl', 'Int', 'Blocks', 'xG', 'npxG', 'xAG', 'SCA', 'GCA', 'Cmp_x', 'Att_x', 'Cmp_pct_x', 'PrgP', 'Carries', 'PrgC', 'Att_x_alt', 'Succ', 'SoTA', 'GA', 'Saves', 'Save_pct', 'PSxG', 'Cmp_y', 'Att_y', 'Cmp_pct_y', 'Att_GK', 'Thr', 'Launch_pct', 'AvgLen', 'Opp')
+    search_fields = ['Player', 'Nation', 'Pos']
+    list_filter = ['Nation', 'Pos']
+    ordering = ['Player']
+    list_per_page = 20
