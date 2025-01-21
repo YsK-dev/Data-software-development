ed193128f80b1e97ea7604c7fc5c9cc78a3d4319
Enes EKİNCİ <211805030@stu.adu.edu.tr>
2025-01-21 18:48:57 +0300

    last updates

--- soft_eng/soft_eng/urls.py
+++ soft_eng/soft_eng/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin # type: ignore
 from django.urls import path, include # type: ignore
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),  
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
